e87ba9879a00f0473cf289611b88ddfe54e232e3
mahesh <heyitssyam@gmail.com>
2024-01-21 16:04:51 -0600

    Hello world in java

---
+++ helloWorld.java
@@ -0,0 +1,27 @@
+public class helloWorld {
+    public static void main(String args[]){
+        System.out.println("Hi Mahesh good");
+     
+        int num =3;
+        System.out.println(num);
+
+        float average = 10.06f;
+        System.out.println(average);
+        byte x = 1;
+        short y =-10;
+        System.out.println(x);
+        System.out.println(y);
+        char c = 'm';
+        System.out.println(c);
+
+        // Type conversion and type casting
+        int b = 300;
+        byte z = (byte)b;
+        System.out.println(z);
+        byte b1=5,b2=6;
+
+byte b3=(byte)(b1+b2);
+
+System.out.println(b3);
+    }
+}
