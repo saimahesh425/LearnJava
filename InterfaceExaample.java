5e4779c6a6068eb977630abd0e1f34328dfc1b3c
mahesh <heyitssyam@gmail.com>
2024-01-27 23:59:54 -0600

    interface example

---
+++ InterfaceExaample.java
@@ -0,0 +1,35 @@
+package out.production.Java;
+
+public class InterfaceExaample {
+    
+    public static void main(String args[]){
+        Computer lap = new laptop();
+        Computer desk = new Desktop();
+        developer dev = new developer();
+        dev.webapp(desk);
+
+    }
+}
+
+interface Computer{
+    public void code();
+}
+
+class laptop implements Computer{
+    public void code(){
+        System.out.println("code, compile, run on laptop");
+    }
+}
+
+class Desktop implements Computer{
+    public void code(){
+        System.out.println("code, compile, run on Desktop");
+    }
+}
+
+class developer{
+    public void webapp(Computer desk){
+        desk.code();
+    }
+}
+
